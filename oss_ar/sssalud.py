cead90104d699d63bbd625c9859b90b426298691
avdata99 <andres@data99.com.ar>
2024-11-24 22:48:30 -0300

    Update data files with new download dates and revised organization names 2024-11-24

--- oss_ar/sssalud.py
+++ oss_ar/sssalud.py
@@ -13,9 +13,35 @@ logger = logging.getLogger(__name__)
 class ObrasSocialesSSS:
 
     SERVICE_URL = 'https://www.sssalud.gob.ar/descargas/dump.php'
-    params = {'obj': 'listRnosc', 'tipo': 7}
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-    
+    # los tipos van de 1 en adelante, no sabemos bien que significa todavia
+    params = {'obj': 'listRnosc', 'tipo': 0}
+    tipos = {
+        "1": "Agentes del Seguro que aceptan Personal de Servicio Doméstico",
+        "2": "Obras Sociales para Empleados de Monotributistas",
+        "3": "Agentes del Seguro que aceptan de Monotributistas",
+        "4": "Agentes del Seguro con Regimen de Adherentes",
+        "5": "Agentes del Seguro que aceptan Jubilados y Pensionados",
+        "6": "Agentes del Seguro de otra Naturaleza",
+        "7": "Agentes del Seguro Sindicales",
+        "8": "Agentes del Seguro Estatales",
+        "9": "Agentes del Seguro por Convenio",
+        "10": "Agentes del Seguro para el Personal de Dirección",
+        "11": "Agentes del Seguro de Administración Mixta",
+        "12": "Asociaciones de Obras Sociales (A.D.O.S.)",
+        "13": "Agentes del Seguro Ley 21.476",
+        "14": "Agentes del Seguro de Empresas",
+        "15": "Federaciones (Vacio)",
+        "16": "Mutuales",
+        "17": "Agentes del Seguro Universitarios",
+        "18": "Agentes del Seguro por Adhesión",
+        "19": "Agentes del Seguro Ley 23.660, Art.1°, inc. i)"
+    }
+
+    user_agent = (
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 '
+        '(KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    )
+
     here = os.path.dirname(os.path.realpath(__file__))
     base_folder = os.path.join(here, 'data')
     # they use XLS extension but it's a TSV file
@@ -38,22 +64,23 @@ class ObrasSocialesSSS:
 
             return self.local_json_object.get(rnos, {})
 
-    def download_databases(self):
+    def download_databases(self, force=False):
         # download all databases
-        logger.info(f'Downloading ALL SSS databases')
+        logger.info('Downloading ALL SSS databases')
         oks = 0
-        for tipo in range(1, 19):
-            ret = self.download_database(tipo_obra_social=tipo)
+        for tipo in self.tipos.keys():
+            ret = self.download_database(tipo_obra_social=tipo, force_download=force)
             if ret:
                 oks += 1
-        
+
         return oks > 10  # ponele
 
     def download_database(self, force_download=False, tipo_obra_social=7):
         """ Download and save database. Return True if OK or None if fails 
             If the file exist we do not re-download. 
-            Use force_download for change this behaviour """
-        
+            Use force_download for change this behaviour
+        """
+
         logger.info(f'Downloading SSS database tipo {tipo_obra_social}')
 
         excel_path = self.local_excel.format(tipo=tipo_obra_social)
@@ -69,30 +96,22 @@ class ObrasSocialesSSS:
             self.errors.append(error)
             self.raw_response = response.content
             self.status_response = response.status_code
-            
+
             logger.error('{} status:{}'.fomrat(error, self.status_response))
-            
+
             return None
-        
+
         self.status_response = response.status_code
         self.raw_response = response.content
         logger.info('respuesta de PUCO: {} {}'.format(response.status_code, response.content))
 
+        # El contenido tiene encoding 'iso-8859-1'
         f = open(excel_path, 'wb')
         f.write(response.content)
         f.close()
 
-        """ no hace falta al parecer
-        # encode_to = 'utf-8'
-        encode_to = 'iso-8859-1'
-        data = response.content.decode(encode_to)
-        f = open('sss2.xls', 'w')
-        f.write(data)
-        f.close()
-        """
-
         return True
-    
+
     def process_database(self, force=False):
         """ read the excel file, clean an return/write a nice json file """
 
@@ -107,28 +126,26 @@ class ObrasSocialesSSS:
                 'localidad', 'cp', 'provincia', 'telefono',
                 'otros_telefonos', 'e_mail', 'web'
             ]
-        
+
         real_rows = {}
 
-        for tipo in range(1, 19):
+        for tipo in self.tipos.keys():
             excel_path = self.local_excel.format(tipo=tipo)
             f = open(excel_path, encoding='ISO-8859-1')
             # the file has 3 bad rows at start
             next(f)  # NEVER
-            next(f)  #   PUT 
-            next(f)  #     BLANK LINES AT CSVs
-            
+            next(f)  # PUT
+            next(f)  # BLANK LINES AT CSVs
+
             # the headers are bad. e.g. _web_ is at _otros_telefonos_
-            
             reader = csv.DictReader(f, delimiter='\t', fieldnames=fieldnames)
-            
-            errors = []
+
             for row in reader:
                 # FIX THE SHIT
                 row['web'] = row.get('otros_telefonos', '')
                 if type(row['web']) == str:
                     row['web'] = row['web'].strip()
-                
+
                 row['e_mail'] = row.get('telefono', '')
                 if type(row['e_mail']) == str:
                     row['e_mail'] = row['e_mail'].strip()
@@ -144,7 +161,7 @@ class ObrasSocialesSSS:
                 row['cp'] = row.get('localidad', '')
                 if type(row['web']) == str:
                     row['cp'] = row['cp'].strip()
-                
+
                 row['localidad'] = ''
                 row['otros_telefonos'] = ''
 
@@ -165,7 +182,7 @@ class ObrasSocialesSSS:
                 else:
                     # DUPLICATED ERROR!
                     self.errors.append(f'Duplicated RNOS: {rnos}')
-            
+
             f.close()
 
         f2 = open(self.local_json, 'w')
@@ -175,7 +192,7 @@ class ObrasSocialesSSS:
         self.local_json_object = real_rows
         self.processed = True
         return real_rows
-    
+
     def count_by_province(self):
         """ conut by province """
         ret = {}
